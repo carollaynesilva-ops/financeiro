3524de486234427ed1f606c4aa389d39131c6446
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2025-12-04 16:50:52 -0300

    terminei

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $usuario_nome = $_SESSION['usuario_nome'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Financeiro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Sistema Financeiro</h1>
@@ -28,7 +29,7 @@ $usuario_nome = $_SESSION['usuario_nome'];
         <p>Bem-vindo. <strong> <?php echo $usuario_nome?> </strong></p>
     
 
-        <a href="logout.php">Sair</a>
+        <a href="logout.php" class="sair">Sair</a>
     </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -21,24 +21,28 @@ if (isset($_SESSION['usuario_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login- Sistema Financeiro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <h1>Login - Sistema Financeiro</h1>
+    <br>
+    <br>
     <?php exibir_mensagem();?>
     <form action="autenticar.php" method="post">
-
+        <br>
+        <br>
         <div>
             <label for="email">E-mail</label>
             <input type="email" name="email" id="email" required>
         </div>
-
+        <br>
         <div>
             <label for="email">Senha:</label>
             <input type="password" name="senha" id="senha" required>
         </div>
-
+        <br>
         <div>
             <button type="submit">
                 Entrar
